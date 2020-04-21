0f905ed24c5d037659ba64d0e4269166b08607bd
WEI-Yi94 <sylvie.wy@gmail.com>
2020-04-21 16:58:18 +0200

    fix path bug and remove xls files generated by programme

--- python/TimeSeries/Stage2020/TimeSeriesTools/recover_xls_files.py
+++ python/TimeSeries/Stage2020/TimeSeriesTools/recover_xls_files.py
@@ -4,12 +4,13 @@ import io
 import glob
 from xlwt import Workbook
 
-def fixFiles(path):
-    all_files =  glob.glob(os.path.join(path,"*.xls"))
+def fixFiles(path_origin, path_destination):
+    all_files =  glob.glob(os.path.join(path_origin,"*.xls"))
     lf = []
     lh = []
     xldoc = Workbook()
     sheet = xldoc.add_sheet("Sheet1", cell_overwrite_ok=True)
+    print("start fixing xls files...")
     for index,filename in enumerate(all_files,start=0):
         print(filename)
         file = io.open(filename, "r", encoding="latin-1")
@@ -23,7 +24,7 @@ def fixFiles(path):
     # Getting the values after splitting using '\t'
             for j, val in enumerate(row.replace('\n', '').split('\t')):
                 sheet.write(i, j, val)
-        filePath= path + r'/myexcel_'+str(index)+'.xls'
+        filePath= path_destination + r'/myexcel_'+str(index)+'.xls'
         xldoc.save(filePath)
     return lh
 
@@ -41,10 +42,17 @@ def convertFiles(data,head,path_destination):
         path = path_destination + r'/myTestCsv_'+str(i)+'.csv'
         file.to_csv(path,index=False)
         
+def delete_xls(path):
+    test = os.listdir(path)
+    for item in test:
+        if item.endswith(".xls"):
+            os.remove(os.path.join(path, item))
+
 if __name__ == "__main__":
     path_origin = r'/home/ymo/local/work-ref/data/Lacq/jour_1'
-    lh=fixFiles(path_origin)
     path_destination = r'/home/ymo/local/work-ref/data/Lacq/jour_1_DT'
+    lh=fixFiles(path_origin,path_destination)
     test=readFiles(path_destination)
     head=lh[0][9].replace('\n','').split('\t')
     convertFiles(test,head,path_destination)
+    delete_xls(path_destination)
